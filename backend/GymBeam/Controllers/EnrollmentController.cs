c5fa922a9d48fdc6b4d626695c1239ea5bdbe070
tirey93 <tirey93@gmail.com>
2024-03-21 08:45:56 +0100

    Added new controller

---
+++ backend/GymBeam/Controllers/EnrollmentController.cs
@@ -0,0 +1,66 @@
+﻿using GymBeam.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymBeam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnrollmentController : ControllerBase
+    {
+        [HttpGet("ByUserId/{id:int}")]
+        public ActionResult<IEnumerable<EnrollmentResponse>> ByUserId(int id)
+        {
+            return new List<EnrollmentResponse>
+            {
+                new EnrollmentResponse
+                {
+                    LeaderId = 2,
+                    ActivityId = 45,
+                    SlotsTaken = 18,
+                    TotalCapacity = 20,
+                    StartTime = DateTime.Now.AddDays(2),
+                    Duration = 40,
+                    Name = "Boks",
+                    ShortDescription = "Short test description.",
+                    LongDescription = "Looooooooooooooong test description.",
+                    LeaderName = "Leader test name"
+                },
+            };
+        }
+
+        [HttpGet("ByDates")]
+        public ActionResult<IEnumerable<EnrollmentResponse>> GetByDates(DateTime from, DateTime to)
+        {
+            return new List<EnrollmentResponse>
+            {
+                new EnrollmentResponse
+                {
+                    ActivityId = 45,
+                    LeaderId = 2,
+                    SlotsTaken = 15,
+                    TotalCapacity = 20,
+                    StartTime = from,
+                    Duration = 40,
+                    Name = "Boks",
+                    ShortDescription = "Short test description.",
+                    LongDescription = "Looooooooooooooong test description.",
+                    LeaderName = "Leader test name"
+                },
+                new EnrollmentResponse
+                {
+                    LeaderId = 2,
+                    ActivityId = 45,
+                    SlotsTaken = 15,
+                    TotalCapacity = 20,
+                    StartTime = to,
+                    Duration = 40,
+                    Name = "Boks",
+                    ShortDescription = "Short test description.",
+                    LongDescription = "Looooooooooooooong test description.",
+                    LeaderName = "Leader test name"
+                },
+            };
+        }
+    }
+}
