fc8117959d249f6bd161bb43dd632a753fc1734a
lukasz-kkk <lukaluzny17@gmail.com>
2024-03-30 11:46:14 +0100

    Deleted redundant userRequest

--- backend/GymBeam/Requests/LoginRequest.cs
+++ backend/GymBeam/Requests/LoginRequest.cs
@@ -12,12 +12,12 @@ namespace GymBeam.Requests
     {
         public LoginRequestValidator()
         {
-            RuleFor(LoginRequest => LoginRequest.Username)
+            RuleFor(loginRequest => loginRequest.Username)
                 .NotEmpty().WithMessage("Username is required.")
                 .MinimumLength(5).WithMessage("Username must be at least 5 characters long.")
                 .MaximumLength(20).WithMessage("Username must not exceed 20 characters.");
 
-            RuleFor(LoginRequest => LoginRequest.Password)
+            RuleFor(loginRequest => loginRequest.Password)
                 .NotEmpty().WithMessage("Password is required.")
                 .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
                 .MaximumLength(20).WithMessage("Password must not exceed 20 characters.");
