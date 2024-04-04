61c633c07a0df81644c664c4ed0019c1aed2d307
Hubert Cywka <hcywka@gmail.com>
2024-04-04 22:59:47 +0200

    remove navigation header

--- frontend/client/src/features/navigation/Shell/AppNavigation.tsx
+++ frontend/client/src/features/navigation/Shell/AppNavigation.tsx
@@ -1,14 +1,13 @@
 import { PropsWithChildren } from "react";
-import { AppShell, Burger, Group } from "@mantine/core";
+import { AppShell, Burger } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
-import { Footer, LanguageSelect, SideNavigation } from "./components";
+import { Footer, SideNavigation } from "./components";
 
 import classes from "./AppNavigation.module.scss";
 
-const NAVIGATION_HEADER_HEIGHT = 60;
 const NAVIGATION_FOOTER_HEIGHT = 80;
-export const NAVIGATION_SHELL_TOTAL_HEIGHT = NAVIGATION_HEADER_HEIGHT + NAVIGATION_FOOTER_HEIGHT;
+export const NAVIGATION_SHELL_TOTAL_HEIGHT = NAVIGATION_FOOTER_HEIGHT;
 
 export const AppNavigation = ({ children }: PropsWithChildren) => {
     const [isNavbarVisible, { toggle: toggleNavbar, close: closeNavbar }] = useDisclosure(false);
@@ -17,24 +16,12 @@ export const AppNavigation = ({ children }: PropsWithChildren) => {
         <AppShell
             withBorder={false}
             layout="alt"
-            header={{ height: NAVIGATION_HEADER_HEIGHT }}
             footer={{ height: NAVIGATION_FOOTER_HEIGHT }}
             navbar={{ width: 100, breakpoint: "sm", collapsed: { desktop: false, mobile: !isNavbarVisible } }}
         >
-            <AppShell.Header className={classes.appNavigationHeader}>
-                <Group className={classes.headerContentContainer}>
-                    <Burger
-                        className={classes.burger}
-                        opened={isNavbarVisible}
-                        onClick={toggleNavbar}
-                        hiddenFrom="sm"
-                    />
-                    <LanguageSelect />
-                </Group>
-            </AppShell.Header>
+            <Burger className={classes.burger} opened={isNavbarVisible} onClick={toggleNavbar} hiddenFrom="sm" />
 
             <AppShell.Navbar className={classes.appNavigationNavbar} withBorder>
-                <Burger className={classes.burger} opened={isNavbarVisible} onClick={toggleNavbar} hiddenFrom="sm" />
                 <SideNavigation onNavigate={closeNavbar} />
             </AppShell.Navbar>
 
