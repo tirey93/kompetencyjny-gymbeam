dce12c358736c8f09b6a7b00fbda0954db63a5ea
lukasz-kkk <lukaluzny17@gmail.com>
2024-04-06 21:41:33 +0200

    Added GetActivityQuery/Handler

---
+++ backend/GymBeam/Queries/GetActivityQuery.cs
@@ -0,0 +1,10 @@
+﻿using GymBeam.Responses;
+using MediatR;
+
+namespace GymBeam.Queries
+{
+    public class GetActivityQuery : IRequest<ActivityResponse>
+    {
+        public int ActivityId { get; set; }
+    }
+}
