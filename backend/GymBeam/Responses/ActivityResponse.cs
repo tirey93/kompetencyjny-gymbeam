66357c604e0d09d974a3811b0d1ef8a136672ecc
lukasz-kkk <lukaluzny17@gmail.com>
2024-04-17 17:54:44 +0200

    Converted not required activity attributes to nullable

--- backend/GymBeam/Responses/ActivityResponse.cs
+++ backend/GymBeam/Responses/ActivityResponse.cs
@@ -3,11 +3,11 @@
     public class ActivityResponse
     {
         public int Id { get; set; }
-        public int Duration { get; set; }
-        public int TotalCapacity { get; set; }
+        public int? Duration { get; set; }
+        public int? TotalCapacity { get; set; }
         public int? LeaderId { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
