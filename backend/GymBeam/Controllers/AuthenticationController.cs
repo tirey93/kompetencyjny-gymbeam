2dacf729666864145bff1e1534d21396638faf0e
lukasz-kkk <lukaluzny17@gmail.com>
2024-11-26 15:25:04 +0100

    Initial endpoints for google oauth

--- backend/GymBeam/Controllers/AuthenticationController.cs
+++ backend/GymBeam/Controllers/AuthenticationController.cs
@@ -14,6 +14,8 @@ using Microsoft.AspNetCore.Mvc;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Security.Claims;
+using System.Text.Json;
+using System.Net.Http.Headers;
 
 namespace GymBeam.Controllers
 {
@@ -28,6 +30,76 @@ namespace GymBeam.Controllers
         {
             _configuration = configuration;
             _mediator = mediator;
+        }
+
+        [HttpGet("google")]
+        public IActionResult GetGoogleLoginLink()
+        {
+            var googleOAuthUri = "https://accounts.google.com/o/oauth2/v2/auth";
+
+            var clientId = _configuration["GoogleOAuth:ClientId"];
+            var redirectUri = _configuration["GoogleOAuth:RedirectUri"];
+            var state = Guid.NewGuid().ToString();
+
+            var scope = "openid email profile";
+
+            var loginLink = $"{googleOAuthUri}?response_type=code" +
+                            $"&client_id={Uri.EscapeDataString(clientId)}" +
+                            $"&redirect_uri={Uri.EscapeDataString(redirectUri)}" +
+                            $"&scope={Uri.EscapeDataString(scope)}" +
+                            $"&state={Uri.EscapeDataString(state)}";
+
+            return Ok(new { link = loginLink });
+        }
+
+
+        [HttpGet("google/callback")]
+        public async Task<IActionResult> GoogleCallback([FromQuery] string code)
+        {
+            // Validate the authorization code
+            if (string.IsNullOrEmpty(code))
+                return BadRequest("Authorization code not provided.");
+
+            using var client = new HttpClient();
+
+            // Exchange the authorization code for an access token
+            var tokenResponse = await client.PostAsync("https://oauth2.googleapis.com/token", new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "code", code },
+                { "client_id", _configuration["GoogleOAuth:ClientId"] },
+                { "client_secret", _configuration["GoogleOAuth:ClientSecret"] },
+                { "redirect_uri", _configuration["GoogleOAuth:RedirectUri"] },
+                { "grant_type", "authorization_code" }
+            }));
+
+            if (!tokenResponse.IsSuccessStatusCode)
+                return BadRequest("Failed to exchange authorization code for token.");
+
+            // Parse the token response
+            var tokenJson = await tokenResponse.Content.ReadAsStringAsync();
+            var tokenData = JsonSerializer.Deserialize<JsonElement>(tokenJson);
+            var accessToken = tokenData.GetProperty("access_token").GetString();
+
+            // Use the access token to retrieve user information
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var userInfoResponse = await client.GetAsync("https://www.googleapis.com/oauth2/v2/userinfo");
+
+            if (!userInfoResponse.IsSuccessStatusCode)
+                return BadRequest("Failed to fetch user information.");
+
+            // Parse the user information response
+            var userJson = await userInfoResponse.Content.ReadAsStringAsync();
+            var userInfo = JsonSerializer.Deserialize<JsonElement>(userJson);
+
+            // Extract relevant user details
+            var email = userInfo.GetProperty("email").GetString();
+            var name = userInfo.GetProperty("name").GetString();
+            var id = userInfo.GetProperty("id").GetString();
+
+            // Log and return user details
+            Console.WriteLine($"User Info: Email = {email}, Name = {name}, Id = {id}");
+
+            return Ok(new { email, name, id });
         }
 
         [HttpPost("Register")]
