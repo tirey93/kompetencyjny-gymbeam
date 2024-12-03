2c3f41b6f71264ab91876ae55a5989fe59683b7e
Damian Opolski <tirey93@gmail.com>
2024-12-03 11:42:21 +0100

    Added Stripe to backend

---
+++ backend/GymBeam/Controllers/SubscriptionController.cs
@@ -0,0 +1,85 @@
+﻿using MediatR;
+using GymBeam.Properties;
+using GymBeam.Queries;
+using GymBeam.Requests;
+using GymBeam.Response;
+using GymBeam.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Stripe;
+namespace GymBeam.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubscriptionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMediator _mediator;
+        private readonly PaymentIntentService _paymentIntentService;
+
+        public SubscriptionController(IConfiguration configuration, IMediator mediator, PaymentIntentService paymentIntentService)
+        {
+            _configuration = configuration;
+            _mediator = mediator;
+            _paymentIntentService = paymentIntentService;
+        }
+        [HttpPost("PaymentIntent")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+#if !DEBUG
+        [Authorize(Roles = Roles.User)]
+#endif
+        public async Task<ActionResult> Post(int userId)
+        {
+            try
+            {
+                var options = new PaymentIntentCreateOptions
+                {
+                    Description = $"userId={userId}",
+                    Currency = "pln",
+                    Amount = 1099,
+                    AutomaticPaymentMethods = new()
+                    {
+                        Enabled = true,
+                    }
+                };
+
+                var paymentIntent = await _paymentIntentService.CreateAsync(options);
+                return Ok(new {clientSecret = paymentIntent.ClientSecret });
+            }
+             catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError,
+                    string.Format(Resource.ControllerInternalError, ex.Message));
+            }
+        }
+
+        [HttpPost("Webhook")]
+        public async Task<IActionResult> Webhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            var signatureHeader = Request.Headers["Stripe-Signature"];
+
+            try
+            {
+                var webhookSecret = Environment.GetEnvironmentVariable(_configuration["StripeAuth:WebhookSigningEnvironmentVariableName"]);
+                var stripeEvent = EventUtility.ConstructEvent(json, signatureHeader, webhookSecret);
+
+                if (stripeEvent.Type == "payment_intent.succeeded")
+                {
+                    var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Ok();
+        }
+
+
+    }
+
+}
---
+++ backend/GymBeam/Extensions/StripeExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+using Stripe;
+
+namespace GymBeam.Extensions
+{
+    public static class StripeExtensions
+    {
+        public static void AddStripe(this IServiceCollection services, string envVariable)
+        {
+            var signingKey = Environment.GetEnvironmentVariable(envVariable);
+            StripeConfiguration.ApiKey = signingKey;
+            services.AddSingleton<PaymentIntentService>();
+
+        }
+    }
+}
