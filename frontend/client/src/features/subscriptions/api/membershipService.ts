30b7b4fcdd054091f7187ae0685331a5260704f3
Hubert Cywka <hcywka@gmail.com>
2024-12-04 22:39:41 +0100

    Minor fixes

--- frontend/client/src/features/subscriptions/api/membershipService.ts
+++ frontend/client/src/features/subscriptions/api/membershipService.ts
@@ -1,8 +1,8 @@
 import { request } from "@/api";
 import { PaymentIntent } from "@/features/subscriptions/api/types";
 
-export class SubscriptionsService {
-    public static startPaymentProcess(): Promise<PaymentIntent> {
+export class MembershipService {
+    public static order(): Promise<PaymentIntent> {
         return request<PaymentIntent>("Subscription/PaymentIntent", { method: "POST" });
     }
 }
