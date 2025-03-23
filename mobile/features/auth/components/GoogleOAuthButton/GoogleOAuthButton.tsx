881a7b1ac6e325acceb374eeaa5f61256a1a5f76
lukasz-kkk <lukaluzny17@gmail.com>
2025-03-23 12:54:45 +0100

    Added oauth

--- mobile/features/auth/components/GoogleOAuthButton/GoogleOAuthButton.tsx
+++ mobile/features/auth/components/GoogleOAuthButton/GoogleOAuthButton.tsx
@@ -1,11 +1,14 @@
+import * as WebBrowser from "expo-web-browser";
 import { Button } from "tamagui";
 
 import { AuthService } from "@/features/auth";
 
+WebBrowser.maybeCompleteAuthSession();
+
 export const GoogleOAuthButton = () => {
     const beginSignInWithGoogle = async () => {
         const { link } = await AuthService.signInWithGoogle();
-        window.location.href = link;
+        await WebBrowser.openAuthSessionAsync(link);
     };
 
     return <Button onPress={beginSignInWithGoogle}>Continue with Google</Button>;
