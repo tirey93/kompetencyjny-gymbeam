3ccd243c423f38864fb3f4012da803320da11dbc
lukasz-kkk <lukaluzny17@gmail.com>
2025-03-23 13:25:48 +0100

    added error handling

--- mobile/features/auth/components/GoogleOAuthButton/GoogleOAuthButton.tsx
+++ mobile/features/auth/components/GoogleOAuthButton/GoogleOAuthButton.tsx
@@ -1,15 +1,31 @@
+import { router } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
 import { Button } from "tamagui";
 
+import { HttpErrorsMap, mapErrorToErrorMessage } from "@/api";
+import { Screens } from "@/constants/Screens";
 import { AuthService } from "@/features/auth";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export const GoogleOAuthButton = () => {
     const beginSignInWithGoogle = async () => {
-        const { link } = await AuthService.signInWithGoogle();
-        await WebBrowser.openAuthSessionAsync(link);
+        try {
+            const { link } = await AuthService.signInWithGoogle();
+            const result = await WebBrowser.openAuthSessionAsync(link);
+            if (result.type === "success") {
+                router.push(Screens.SignIn);
+            }
+        } catch (error) {
+            const errorMessage = mapErrorToErrorMessage(error, errorsMap);
+            throw new Error(errorMessage);
+        }
     };
 
     return <Button onPress={beginSignInWithGoogle}>Continue with Google</Button>;
+};
+
+const errorsMap: HttpErrorsMap = {
+    defaultError: "Unknown error.",
+    statusCodesMap: {},
 };
