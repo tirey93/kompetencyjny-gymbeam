99871ab2a9892336bf43f703ba8f4f3c23dc7f7d
lukasz-kkk <lukaluzny17@gmail.com>
2025-04-27 18:46:27 +0200

    deleted debug traces

--- mobile/features/auth/components/GoogleOAuthButton/GoogleOAuthButton.tsx
+++ mobile/features/auth/components/GoogleOAuthButton/GoogleOAuthButton.tsx
@@ -2,18 +2,14 @@ import { router } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
 import { Button } from "tamagui";
 import { toast } from "sonner-native";
-import * as Linking from 'expo-linking';
 
 import { HttpErrorsMap, mapErrorToErrorMessage } from "@/api";
 import { Screens } from "@/constants/Screens";
 import { AuthService, useAuthState, useSignIn } from "@/features/auth";
 import * as AuthSession from "expo-auth-session";
-import { string } from "yup";
-import { useEffect } from "react";
 import { useAppOverlay } from "@/hooks/useAppOverlay";
 import { UserService } from "@/features/users";
 import { mapUserDtoToUser } from "@/features/users/utils/mapUserDtoToUser";
-import { useReloadUser } from "@/features/users/hooks/useReloadUser";
 import { setAuthCookie } from "@/api/apiRequest";
 
 WebBrowser.maybeCompleteAuthSession();
@@ -51,12 +47,6 @@ export const GoogleOAuthButton = () => {
                 const params = new URLSearchParams(url.search);
                 const authToken = params.get('authToken');
                 const userId = params.get('userId');
-                // const uset = 
-                console.log("authToken")
-                console.log(authToken);
-            
-                console.log("userId")
-                console.log(userId);
 
                 if (authToken && userId) {
                     setAuthCookie(authToken, userId);
